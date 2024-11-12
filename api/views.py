38020b065f6982e71a7bd90d230f4fdd9849ffe2
praveen <nairxprv@gmail.com>
2024-11-12 10:19:14 +0530

    added products

--- api/views.py
+++ api/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from api.models import product
+from .serializers import ProductSerializer
 
 @api_view()
 def getData(request):
     return Response({"message":"Hello World"})
+
+class ProductList(APIView):
+    def get(self,request):
+        products = product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+            # return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    
