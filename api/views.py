d968a69ba76e0d662f418e6babf7015cc8f30bb4
nairx <nairxprv@gmail.com>
2024-12-13 19:03:23 +0530

    added

--- api/views.py
+++ api/views.py
@@ -7,17 +7,16 @@ from rest_framework import status
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated 
 
-
 @api_view()
-@permission_classes((IsAuthenticated,))
+# @permission_classes((IsAuthenticated,))
 def getData(request):
     return Response({"message":"Hello World"})
 
-@permission_classes((IsAuthenticated,))
+# @permission_classes((IsAuthenticated,))
 class ProductList(APIView):
     def get(self,request):
         products = product.objects.all()
-        serializer = ProductSerializer(products,many=True)
+        serializer = ProductSerializer(products,many=True) #converted to json
         return Response(serializer.data)
     def post(self,request):
         serializer = ProductSerializer(data=request.data)
@@ -26,7 +25,7 @@ class ProductList(APIView):
             return Response(serializer.data,status=200)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
-@permission_classes((IsAuthenticated,))
+# @permission_classes((IsAuthenticated,))
 class ProductDetail(APIView):
     def get_object(self, pk):
         try:
